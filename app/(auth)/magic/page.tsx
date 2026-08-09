c1d48d1f5419d03246f49b286abcd36ba0cd9d29
Sergey Dubskiy <dubskiy_03@mail.ru>
2026-08-09 13:40:44 +0300

    repair MagicConfirmPage

--- app/(auth)/magic/page.tsx
+++ app/(auth)/magic/page.tsx
@@ -3,21 +3,28 @@
 import Loader from '@/features/loader/components/Loader';
 import { useConfirmMagic } from '@/features/auth/hooks/useConfirmMagic';
 import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 
-const MagicConfirmPage = () => {
+const MagicConfirmContent = () => {
    const { mutate: confirmMagic } = useConfirmMagic();
    const searchParams = useSearchParams();
    const token = searchParams.get('token');
 
    useEffect(() => {
-      console.log(token);
       if (token) {
          confirmMagic({ token });
       }
-   }, [token]);
+   }, [token, confirmMagic]);
 
    return <Loader visible={true} />;
+};
+
+const MagicConfirmPage = () => {
+   return (
+      <Suspense fallback={<Loader visible={true} />}>
+         <MagicConfirmContent />
+      </Suspense>
+   );
 };
 
 export default MagicConfirmPage;
